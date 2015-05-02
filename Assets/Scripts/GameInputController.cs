e1e028f5381e0a5f377f4b2363d1bedb61947d2f
TerryKatsoulis <terry.katsoulis15@gmail.com>
2015-05-01 21:12:06 -0400

    pull this

--- Assets/Scripts/GameInputController.cs
+++ Assets/Scripts/GameInputController.cs
@@ -14,17 +14,18 @@ public class GameInputController : MonoBehaviour
 	void Update()
 	{
 		if (UIcontrols.menuOP == true) {
-			if(Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.A) || Input.GetKeyDown(KeyCode.Space)) {
+			if(Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.A) || Input.GetKeyDown(KeyCode.M)) {
 				UIcontrols.menuOP = false;
 			}
-		}
-		if (Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.A)) {
-			cube.RotateX();
 		}
 		if (Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.B) || Input.GetKeyDown(KeyCode.M)) {
 			UIcontrols.menuOP = true;
 			//cube.RotateY();
 		}
+		if (Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.X)) {
+			cube.RotateX();
+		}
+
 		if (Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.X)) {
 			cube.RotateZ();
 		}
