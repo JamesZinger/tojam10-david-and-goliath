ba98d4f110b32a9e8dd5dc27b33f4edbcaf09a15
Smiley7 <danne_assyrian@Hotmail.com>
2015-05-01 20:45:20 -0400

    Added UIimages

--- Assets/Scripts/GameUIController.cs
+++ Assets/Scripts/GameUIController.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameUIController : MonoBehaviour {
 
+	public Image MenuOp;
 	// Use this for initialization
 	void Start () {
-	
+		MenuOp.gameObject.SetActive(false);
 	}
 	
 	// Update is called once per frame
