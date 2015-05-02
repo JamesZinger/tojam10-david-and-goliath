6583b365b7d642edb8579c8522ee6a2eb7e20d8b
Jeff Rose <jrose0@gmail.com>
2015-05-02 15:48:39 -0400

    Animating TheGoat and getting quads prefabbed so they automatically display start and endpoints.

--- Assets/Scripts/Quad.cs
+++ Assets/Scripts/Quad.cs
@@ -4,4 +4,22 @@ public class Quad : MonoBehaviour
 {
 	public string NodeName;
 	public Node Node;
+
+	public void Start()
+	{
+		if ( Node != null )
+		{
+			Transform startpoint = transform.FindChild( "Startpoint" );
+			if ( startpoint != null && Node.Type == NodeTypeEnum.Start )
+			{
+				startpoint.gameObject.SetActive( true );
+			}
+
+			Transform endpoint = transform.FindChild( "Endpoint" );
+			if ( endpoint != null && Node.Type == NodeTypeEnum.End )
+			{
+				endpoint.gameObject.SetActive( true );
+			}
+		}
+	}
 }
