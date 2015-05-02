b30dbf7fd2fdd273e857ca64cb36d8477245e207
TerryKatsoulis <terry.katsoulis15@gmail.com>
2015-05-01 20:27:59 -0400

    updated some controls

--- Assets/Scripts/GameInputController.cs
+++ Assets/Scripts/GameInputController.cs
@@ -2,13 +2,23 @@
 
 public class GameInputController : MonoBehaviour
 {
+	Cube cube;
+
 	void Start()
 	{
-		 
+		cube = FindObjectOfType<Cube> ();
 	}
 
 	void Update()
 	{
-		
+		if (Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.A)) {
+			cube.RotateX();
+		}
+		if (Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.B)) {
+			cube.RotateY();
+		}
+		if (Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.X)) {
+			cube.RotateZ();
+		}
 	}
 }
