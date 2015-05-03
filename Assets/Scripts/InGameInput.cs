ae04e2dc3428d7e3feeb51bd3653a198882ea088
TerryKatsoulis <terry.katsoulis15@gmail.com>
2015-05-02 21:26:26 -0400

    new controls schemes

--- Assets/Scripts/InGameInput.cs
+++ Assets/Scripts/InGameInput.cs
@@ -6,6 +6,8 @@ public class InGameInput : MonoBehaviour {
 
 	public EventSystem es;
 	private int opSel = 0;
+	//private int winSel = 0;
+
 	Cube cube;
 	private InGameUI UIcontrols;
 	// Use this for initialization
@@ -60,6 +62,49 @@ public class InGameInput : MonoBehaviour {
 			}
 			return;
 		}
+
+		/*if (UIcontrols.winScreen.gameObject.activeSelf == true) {
+			// When menu is active the following is allowed
+			if (Xbox360GamepadState.Instance.AxisJustPastThreshold(Xbox.Axis.LAnalogY, -0.5f) || Input.GetKeyDown(KeyCode.S)) {
+				winSel +=1; // Increment the array by 1 as you move down the list
+				// This statement allows for returning back to top option once you reach the bottom of the list
+				if (UIcontrols.gameOptions.Length == winSel)
+				{
+					// Sets the variable for the array back to 0 to continue moving down the list
+					winSel = 0;
+				}
+				// This line is what allows for the selection of buttons through key input and so on
+				es.SetSelectedGameObject(UIcontrols.winOptions[winSel].gameObject, new BaseEventData(es));
+			}
+			// This statement does the same as above but for upward action
+			if (Xbox360GamepadState.Instance.AxisJustPastThreshold(Xbox.Axis.LAnalogY, 0.5f) || Input.GetKeyDown(KeyCode.W)) {
+				winSel -=1;
+				if (winSel < 0)
+				{
+					winSel = 1;
+				}
+				es.SetSelectedGameObject(UIcontrols.winOptions[winSel].gameObject, new BaseEventData(es));
+			}
+			if ((winSel == 0 && Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.A))|| (winSel == 0 && Input.GetKeyDown(KeyCode.A))) {
+				UIcontrols.winScreen.gameObject.SetActive (false);
+					Application.LoadLevel(1);
+			}
+
+			if(Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.A) || Input.GetKeyDown(KeyCode.Space)) {
+				UIcontrols.winScreen.gameObject.SetActive(false);
+				Application.LoadLevel(0);
+			}
+			if (Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.Start) || Input.GetKeyDown(KeyCode.M)) {
+				UIcontrols.winScreen.gameObject.SetActive(true);
+				Time.timeScale = 0;
+			}
+			if(Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.Back) || Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.Start)|| Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.B)|| Input.GetKeyDown(KeyCode.Space)) {
+				UIcontrols.winScreen.gameObject.SetActive(false);
+				Time.timeScale = 1;
+			}
+			return;
+		}*/
+		
 		if (UIcontrols.pauseMenu.gameObject.activeSelf == false) {
 			if (Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.Start) || Input.GetKeyDown(KeyCode.M)) {
 				UIcontrols.pauseMenu.gameObject.SetActive(true);
@@ -67,6 +112,14 @@ public class InGameInput : MonoBehaviour {
 			}
 			//cube.RotateY();
 		}
+		/*if (UIcontrols.winScreen.gameObject.activeSelf == false) {
+			if (Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.Start) || Input.GetKeyDown(KeyCode.M)) {
+				UIcontrols.winScreen.gameObject.SetActive(true);
+				Time.timeScale = 0;
+			}
+			//cube.RotateY();
+		}*/
+		
 		if (UIcontrols.inGameHowTo.gameObject.activeSelf == true) {
 			if (Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.B) || Input.GetKeyDown (KeyCode.Space)) {
 				UIcontrols.inGameHowTo.gameObject.SetActive (false);
@@ -75,6 +128,7 @@ public class InGameInput : MonoBehaviour {
 				es.SetSelectedGameObject(UIcontrols.gameOptions[opSel].gameObject, new BaseEventData(es));
 			}
 		}
+
 		if (UIcontrols.pauseMenu.gameObject.activeSelf == false) {
 			if (Xbox360GamepadState.Instance.IsButtonDown (Xbox.Button.X)) {
 				cube.RotateX ();
--- Assets/Scripts/InGameUI.cs
+++ Assets/Scripts/InGameUI.cs
@@ -8,14 +8,18 @@ public class InGameUI : MonoBehaviour {
 
 	public Image pauseMenu;
 	public Image inGameHowTo;
+	public Image winScreen;
 	public Button[] gameOptions;
+	public Button[] winOptions;
+	private TextMesh tryMesh;
+	Cube cube;
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
