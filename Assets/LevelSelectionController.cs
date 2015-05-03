a104fbab9d4b80327c2ea2b5778bba9bafeda981
James Zinger <joey.zinger.99@gmail.com>
2015-05-03 16:56:52 -0400

    wwhkljhsfdglkhdfsklgsdkflhglkjdlkjgdg

---
+++ Assets/LevelSelectionController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class LevelSelectionController : MonoBehaviour
+{
+	public GameObject[] levels;
+	public AudioSource onClick;
+	private int lvlSel = 0;
+
+	// Use this for initialization
+	void OnEnable ()
+	{
+		lvlSel = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Xbox360GamepadState.Instance.AxisJustPastThreshold( Xbox.Axis.LAnalogX, 0.5f ) || Input.GetKeyDown( KeyCode.D ) )
+		{
+			lvlSel += 1;
+			if ( levels.Length == lvlSel )
+			{
+				lvlSel = 0;
+			}
+			EventSystem.current.SetSelectedGameObject( levels[ lvlSel ].gameObject, new BaseEventData( EventSystem.current ) );
+		}
+		else if ( Xbox360GamepadState.Instance.AxisJustPastThreshold( Xbox.Axis.LAnalogX, -0.5f ) || Input.GetKeyDown( KeyCode.A ) )
+		{
+			lvlSel -= 1;
+			if ( lvlSel < 0 )
+			{
+				lvlSel = levels.Length - 1;
+			}
+			EventSystem.current.SetSelectedGameObject( levels[ lvlSel ].gameObject, new BaseEventData( EventSystem.current ) );
+		}
+		if ( ( lvlSel == 0 && Xbox360GamepadState.Instance.IsButtonDown( Xbox.Button.A ) ) || ( lvlSel == 0 && Input.GetKeyDown( KeyCode.Return ) ) )
+		{
+			onClick.Play();
+			Cube.LevelString = "Level 1";
+			Application.LoadLevel( 1 );
+		}
+		if ( ( lvlSel == 1 && Xbox360GamepadState.Instance.IsButtonDown( Xbox.Button.A ) ) || ( lvlSel == 1 && Input.GetKeyDown( KeyCode.Return ) ) )
+		{
+			onClick.Play();
+			Cube.LevelString = "Level 2";
+			Application.LoadLevel( 1 );
+		}
+		if ( ( lvlSel == 2 && Xbox360GamepadState.Instance.IsButtonDown( Xbox.Button.A ) ) || ( lvlSel == 2 && Input.GetKeyDown( KeyCode.Return ) ) )
+		{
+			onClick.Play();
+			Cube.LevelString = "Level 3";
+			Application.LoadLevel( 1 );
+		}
+		if ( ( lvlSel == 3 && Xbox360GamepadState.Instance.IsButtonDown( Xbox.Button.A ) ) || ( lvlSel == 3 && Input.GetKeyDown( KeyCode.Return ) ) )
+		{
+			onClick.Play();
+			Cube.LevelString = "Level 4";
+			Application.LoadLevel( 1 );
+		}
+		if ( ( lvlSel == 4 && Xbox360GamepadState.Instance.IsButtonDown( Xbox.Button.A ) ) || ( lvlSel == 4 && Input.GetKeyDown( KeyCode.Return ) ) )
+		{
+			onClick.Play();
+			Cube.LevelString = "Level 5";
+			Application.LoadLevel( 1 );
+		}
+		if ( ( lvlSel == 5 && Xbox360GamepadState.Instance.IsButtonDown( Xbox.Button.A ) ) || ( lvlSel == 5 && Input.GetKeyDown( KeyCode.Return ) ) )
+		{
+			onClick.Play();
+			Cube.LevelString = "Level 6";
+			Application.LoadLevel( 1 );
+		}
+		if ( ( lvlSel == 6 && Xbox360GamepadState.Instance.IsButtonDown( Xbox.Button.A ) ) || ( lvlSel == 6 && Input.GetKeyDown( KeyCode.Return ) ) )
+		{
+			onClick.Play();
+			FindObjectOfType<InGameMenuController>().isInChildScreen = false;
+			gameObject.SetActive( false );
+		}
+	}
+}
--- Assets/Scripts/InGameMenuController.cs
+++ Assets/Scripts/InGameMenuController.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Linq;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class InGameMenuController : MonoBehaviour
@@ -9,63 +10,69 @@ public class InGameMenuController : MonoBehaviour
 
 	public float TransitionSpeed = 1;
 	public RectTransform[] children;
+	public GameObject LevelSelection;
+	public GameObject CreditScreen;
+	
 	private Vector2[] targetPositions;
 	private Vector2[] startingPositions;
 	private Button selfButton;
 	private bool isExpanded;
 	private bool isCoroutineRunning;
+	private int selectedIndex = 0;
+	public bool isInChildScreen;
 
-
-	void Awake ()
+	private void Awake()
 	{
 		targetPositions = new Vector2[children.Length];
 
-		for (int i =0; i < children.Length; i++)
+		for ( int i = 0; i < children.Length; i++ )
 		{
 			targetPositions[ i ] = children[ i ].anchoredPosition;
 			children[ i ].anchoredPosition = ( (RectTransform) transform ).anchoredPosition;
-			if ( i != 2 )
+			if ( i != 0 )
 				children[ i ].gameObject.SetActive( false );
 		}
-		children[ 2 ].anchoredPosition = targetPositions[ 0 ];
+		children[ 0 ].anchoredPosition = targetPositions[ 1 ];
 
 		startingPositions = children.Select( t => t.anchoredPosition ).ToArray();
 	}
+
 	// Use this for initialization
-	void Start ()
+	private void Start()
 	{
-		selfButton = children[ 2 ].GetComponent<Button>();
+		selfButton = children[ 0 ].GetComponent<Button>();
 		selfButton.onClick.AddListener( Toggle );
 	}
 
 	public void Toggle()
 	{
 		if ( isCoroutineRunning ) return;
-		StartCoroutine( buttonMover() );
+		StartCoroutine( ButtonMover() );
 	}
 
-	IEnumerator buttonMover ()
+	private IEnumerator ButtonMover()
 	{
 		float progress = 0f;
 		if ( !isExpanded )
 		{
+			Time.timeScale = 0;
 			isCoroutineRunning = true;
-			
+
 			while ( progress < 1f )
 			{
-				children[ 2 ].anchoredPosition = Vector2.Lerp( startingPositions[ 2 ], targetPositions[ 2 ], progress );
-				children[ 1 ].anchoredPosition = children[ 2 ].anchoredPosition;
-				children[ 0 ].anchoredPosition = children[ 2 ].anchoredPosition;
+				children[ 0 ].anchoredPosition = Vector2.Lerp( startingPositions[ 0 ], targetPositions[ 0 ], progress );
+				children[ 1 ].anchoredPosition = children[ 0 ].anchoredPosition;
+				children[ 2 ].anchoredPosition = children[ 0 ].anchoredPosition;
 
-				progress += Time.deltaTime * TransitionSpeed;
+				progress += Time.unscaledDeltaTime * TransitionSpeed;
 				yield return null;
 			}
 
 			for ( int i = 0; i < children.Length; i++ )
 			{
-				children[ i ].anchoredPosition = targetPositions[ 2 ];
+				children[ i ].anchoredPosition = targetPositions[ 0 ];
 			}
-			
+
 			progress = 0f;
 
 			Array.ForEach( children, r => {
@@ -74,11 +81,11 @@ public class InGameMenuController : MonoBehaviour
 
 			while ( progress < 1f )
 			{
-				for ( int i = children.Length - 2; i >= 0; i-- )
+				for ( int i = 1; i < children.Length; i++ )
 				{
-					children[ i ].anchoredPosition = Vector2.Lerp( children[ 2 ].anchoredPosition, targetPositions[ i ], progress );
+					children[ i ].anchoredPosition = Vector2.Lerp( children[ 0 ].anchoredPosition, targetPositions[ i ], progress );
 				}
-				progress += Time.deltaTime * TransitionSpeed;
+				progress += Time.unscaledDeltaTime * TransitionSpeed;
 				yield return null;
 			}
 
@@ -87,47 +94,119 @@ public class InGameMenuController : MonoBehaviour
 				children[ i ].anchoredPosition = targetPositions[ i ];
 			}
 
-
+			EventSystem.current.SetSelectedGameObject( children[ 0 ].gameObject, new BaseEventData( EventSystem.current ) );
 			isExpanded = true;
 		}
 		else
 		{
 			while ( progress < 1f )
 			{
-				for ( int i = children.Length - 2; i >= 0; i-- )
+				for ( int i = 1; i < children.Length; i++ )
 				{
-					children[ i ].anchoredPosition = Vector2.Lerp( targetPositions[ i ], children[ 2 ].anchoredPosition, progress );
+					children[ i ].anchoredPosition = Vector2.Lerp( targetPositions[ i ], children[ 0 ].anchoredPosition, progress );
 				}
-				progress += Time.deltaTime * TransitionSpeed;
+				progress += Time.unscaledDeltaTime * TransitionSpeed;
 				yield return null;
 			}
 
 			for ( int i = 0; i < children.Length; i++ )
 			{
-				if ( i != 2 )
+				if ( i != 0 )
 					children[ i ].gameObject.SetActive( false );
 
-				children[ i ].anchoredPosition = children[ 2 ].anchoredPosition;
+				children[ i ].anchoredPosition = children[ 0 ].anchoredPosition;
 			}
 
 			progress = 0;
 			while ( progress < 1f )
 			{
-				children[ 2 ].anchoredPosition = Vector2.Lerp( targetPositions[ 2 ], startingPositions[ 2 ], progress );
-				children[ 1 ].anchoredPosition = children[ 2 ].anchoredPosition;
-				children[ 0 ].anchoredPosition = children[ 2 ].anchoredPosition;
+				children[ 0 ].anchoredPosition = Vector2.Lerp( targetPositions[ 0 ], startingPositions[ 0 ], progress );
+				children[ 1 ].anchoredPosition = children[ 0 ].anchoredPosition;
+				children[ 2 ].anchoredPosition = children[ 0 ].anchoredPosition;
 
-				progress += Time.deltaTime * TransitionSpeed;
+
+				progress += Time.unscaledDeltaTime * TransitionSpeed;
 				yield return null;
 			}
 
+			Time.timeScale = 1f;
 			isExpanded = false;
 		}
 		isCoroutineRunning = false;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	private void Update()
+	{
+		if ( isCoroutineRunning ) return;
+
+		if ( isExpanded && !isInChildScreen )
+		{
+			if ( Xbox360GamepadState.Instance.AxisJustPastThreshold( Xbox.Axis.LAnalogY, -0.5f ) || Input.GetKeyDown( KeyCode.S ) )
+			{
+				selectedIndex++;
+				if ( selectedIndex == children.Length ) selectedIndex = 0;
+
+				EventSystem.current.SetSelectedGameObject( children[ selectedIndex ].gameObject,
+					new BaseEventData( EventSystem.current ) );
+
+
+			}
+			// This statement does the same as above but for upward action
+			if ( Xbox360GamepadState.Instance.AxisJustPastThreshold( Xbox.Axis.LAnalogY, 0.5f ) || Input.GetKeyDown( KeyCode.W ) )
+			{
+				selectedIndex--;
+				if ( selectedIndex == -1 ) selectedIndex = children.Length - 1;
+
+				EventSystem.current.SetSelectedGameObject( children[ selectedIndex ].gameObject,
+					new BaseEventData( EventSystem.current ) );
+			}
+
+			if ( Xbox360GamepadState.Instance.IsButtonDown( Xbox.Button.Start ) || Input.GetKeyDown( KeyCode.M ) )
+			{
+				Toggle();
+			}
+
+			if ( Xbox360GamepadState.Instance.IsButtonDown( Xbox.Button.A ) || Input.GetKeyDown( KeyCode.A ) )
+			{
+				if ( selectedIndex == 0 )
+				{
+					// Spawn level selection screen
+					Toggle();
+				}
+				else if ( selectedIndex == 1 )
+				{
+					isInChildScreen = true;
+					LevelSelection.SetActive( true );
+				}
+				else if ( selectedIndex == 2 )
+				{
+					isInChildScreen = true;
+					CreditScreen.SetActive( true );
+				}
+			}
+		}
+		else if ( isExpanded && isInChildScreen )
+		{
+			if ( Xbox360GamepadState.Instance.IsButtonDown( Xbox.Button.B ) || Input.GetKeyDown( KeyCode.Space ) )
+			{
+				switch ( selectedIndex )
+				{
+					case 1:
+
+						break;
+					case 2:
+						CreditScreen.SetActive( false );
+						break;
+				}
+			}
+		}
+		else if ( !isExpanded )
+		{
+			if ( Xbox360GamepadState.Instance.IsButtonDown( Xbox.Button.Start ) || Input.GetKeyDown( KeyCode.M ) )
+			{
+				Toggle();
+			}
+		}
 	}
 }
