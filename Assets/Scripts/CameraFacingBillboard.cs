3ace057dd8924b60d6212eaccadb0eaaeb99472b
Jeff Rose <jrose0@gmail.com>
2015-05-02 21:07:12 -0400

    Getting the goat icon to always face the camera and draw on top of all other layers of geometry. Updated materials to be decals so that we can texture the lines onto them easily layer. Added new textures and preliminary music.

---
+++ Assets/Scripts/CameraFacingBillboard.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class CameraFacingBillboard : MonoBehaviour
+{
+    public Camera Camera;
+ 
+    void LateUpdate()
+    {
+        transform.LookAt( Camera.transform.position, Camera.transform.up );
+    }
+}
